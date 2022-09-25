be2f719824057ff5b33f506630178c9f21062afa
Rajajr <rajjr011@gmail.com>
2022-09-26 05:54:50 +0700

    Find Largest Among Three Numbers

---
+++ LargestAmongThree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpandFunctional
+{
+    internal class LargestAmongThree
+    {
+        public static void LargestAmongThreeNumbers()
+        {
+            // Find the Largest Among Three Numbers
+            //variable
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
+            Console.WriteLine("Enter any Three Number ");
+            Console.WriteLine("X");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Y");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Z");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            if (x > y && x > z)
+                Console.WriteLine("Given Number {0} is Largest",x);
+            else if (y > x && x > z)
+                Console.WriteLine("Given Number {0} is Largest",y);
+            else if (z > x && z > y)
+                Console.WriteLine("Given Number {0} is Largest",z);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,9 +10,22 @@ namespace BasicCsharpandFunctional
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter any Letters To Check Vowels Or Constant");
-            string input=Console.ReadLine();
-            VowelsOrConsonant.VowelConstant(input);
+            Console.WriteLine("Please choose any one program from below option");
+            Console.WriteLine("1:Vowel or Consontant\n2:Find Largest Among Three Numbers");
+            int option=Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Enter any Letters To Check Vowels Or Constant");
+                    string input = Console.ReadLine();
+                    VowelsOrConsonant.VowelConstant(input);
+                    break;
+
+                case 2:
+                    LargestAmongThree.LargestAmongThreeNumbers();
+                    break;
+            }
             Console.ReadLine();
         }
     }
