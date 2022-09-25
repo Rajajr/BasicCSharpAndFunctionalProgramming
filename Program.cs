08a351c0e71f9aa0f9cc9d077907903904b4b473
Rajajr <rajjr011@gmail.com>
2022-09-26 04:20:55 +0700

    Odd Or Even Numbers

--- Program.cs
+++ Program.cs
@@ -10,24 +10,19 @@ namespace BasicCsharpandFunctional
     {
         static void Main(string[] args)
         {
-            //1 : Swaping Two Numbers 
+            //2 :Check Wether a Number is Even Or Odd 
             //variables
-            int x = 0;
-            int y = 0;
-            int result = 0;
+            int num = 0;
+          
+            Console.WriteLine("Please enter the Number");
+            num = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the x value");
-            Console.WriteLine("x");
-            int X = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the y value");
-            Console.WriteLine("y");
-            int Y = Convert.ToInt32(Console.ReadLine());
+            if ( num%2 == 0)
 
-            Console.WriteLine("Before Swap x :{0},y :{1}", X, y);
-            result = x;
-            x = y;
-            y = result;
-            Console.WriteLine("After Swap x :{0},y :{1}", x, y);
+                Console.WriteLine("Given Number is Even", num);
+            else
+                Console.WriteLine("Given Number is Odd", num);
+            
             Console.ReadLine();
 
 
