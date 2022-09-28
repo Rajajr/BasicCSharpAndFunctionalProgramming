9b58b384671237bd707923f6425ea1c9c0305131
Rajajr <rajjr011@gmail.com>
2022-09-28 11:50:02 +0700

    Check Weather Leap Year or Not a Leap Year

---
+++ LeapYear.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpandFunctional
+{
+    internal class LeapYear
+    {
+        public static void LeapYearOrNot()
+        {
+            int year = 0;
+            Console.WriteLine("Enter the year :- ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            {
+                Console.WriteLine("Year : {0} is Leap Year", +year);
+            }
+
+            else 
+
+            {
+                Console.WriteLine("Year : {0} is not a Leap Year", +year);
+            }
+            
+
+        }
+    }
+}
+            
+        
